47cef22553348a9de7da8f0c12aa1eb024337878
llandrell <andrelucas85@hotmail.com>
2025-06-26 00:03:54 -0300

    Commit da aula de operadores

---
+++ Operadores/aritimeticos.py
@@ -0,0 +1,24 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2 + 3.5) #adição
+
+print(produto_1 - produto_2 - 3.5) #subtração
+
+print(produto_1 * produto_2 * 3.5) #multiplicação
+
+print(produto_1 / produto_2 / 3.5) #divisão
+
+print(produto_1 // produto_2) #divisão com o resto interio
+
+print(produto_1 % produto_2) # mudulo, resto da divisão
+
+print(produto_1 ** produto_2) #exponenciação
+
+x = 10 + 5 *4 #usando a precedencia
+
+print(x)
+
+xs = (10 + 5) * 4 #forçando a precedencia, ou sejá o que tem nos parenteses soma primeiro 
+
+print(xs)
